3d7ab6407ee86a1ab6ae495445ef6ddba4206ce1
eliasgirma <elias.girma0@gmail.com>
2023-02-27 13:14:40 +0300

    ci: configured vs code launch settings

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,5 +1,6 @@
 command_exists () {
-  command -v "$1" >/dev/null2>&1}
+  command -v "$1" >/dev/null 2>&1
+  }
 
 # Workaround for Windows 10, Git Bash and Yarn
 if command_exists winpty && test -t 1; then
